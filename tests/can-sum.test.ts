7b45e27ccb2bd724ad811786681f2dcc1295c66a
Ronald Fung <rfung.dev@icloud.com>
2024-12-19 23:08:34 +0000

    Added canSum memoization

---
+++ tests/can-sum.test.ts
@@ -0,0 +1,26 @@
+const can_sum = require("../src/can-sum.ts");
+
+// Usage: test('description', testFunction)
+test("canSum(7, [2, 3]) == true", () => {
+	expect(can_sum.canSum(7, [2, 3])).toBe(true);
+});
+
+test("canSum(7, [5, 3, 4, 7]) == true", () => {
+	expect(can_sum.canSum(7, [5, 3, 4, 7])).toBe(true);
+});
+
+test("canSum(7, [2, 4]) == false", () => {
+	expect(can_sum.canSum(7, [2, 4])).toBe(false);
+});
+
+test("canSum(8, [2, 3, 5]) == true", () => {
+	expect(can_sum.canSum(8, [2, 3, 5])).toBe(true);
+});
+
+test("canSum(300, [7, 14]) == false", () => {
+	expect(can_sum.canSum(300, [7, 14])).toBe(false);
+});
+
+test("canSumMem(300, [7, 14]) == false", () => {
+	expect(can_sum.canSumMem(300, [7, 14])).toBe(false);
+});
